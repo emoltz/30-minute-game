c357ea47ba149e26e41c29b8b4b929afcd27a244
Ethan <33405530+emoltz@users.noreply.github.com>
2022-03-29 19:50:13 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
+function createEnemy () {
+    enemySprite1 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    animation.runImageAnimation(
+    enemySprite1,
+    assets.animation`myAnim0`,
+    200,
+    true
+    )
+    tiles.placeOnRandomTile(enemySprite1, sprites.dungeon.buttonOrangeDepressed)
+}
+let enemySprite1: Sprite = null
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -27,4 +55,5 @@ tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 8))
 game.onUpdate(function () {
     controller.moveSprite(mySprite, 200, 200)
     scene.cameraFollowSprite(mySprite)
+    createEnemy()
 })
