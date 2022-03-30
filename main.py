28baaa72d765f68c75dfd50407d87b20bc3ec455
Ethan <33405530+emoltz@users.noreply.github.com>
2022-03-29 20:18:19 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
+def createEnemy():
+    global enemySprite1
+    enemySprite1 = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.enemy)
+    animation.run_image_animation(enemySprite1,
+        assets.animation("""
+            myAnim0
+        """),
+        200,
+        True)
+    tiles.place_on_random_tile(enemySprite1, sprites.dungeon.button_orange_depressed)
+    enemySprite1.follow(mySprite, 50)
+
+def on_on_overlap(sprite, otherSprite):
+    music.siren.play()
+    game.over(False, effects.slash)
+sprites.on_overlap(SpriteKind.enemy, SpriteKind.player, on_on_overlap)
+
+enemySprite1: Sprite = None
+mySprite: Sprite = None
 mySprite = sprites.create(img("""
         . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -20,7 +57,19 @@ mySprite = sprites.create(img("""
 animation.run_image_animation(mySprite, assets.animation("""
     myAnim
 """), 200, True)
+tiles.set_current_tilemap(tilemap("""
+    level1
+"""))
+tiles.place_on_tile(mySprite, tiles.get_tile_location(8, 8))
+createEnemy()
 
 def on_on_update():
-    pass
+    controller.move_sprite(mySprite, 200, 200)
+    scene.camera_follow_sprite(mySprite)
+    if game.runtime() >= 10000:
+        game.over(True, effects.confetti)
 game.on_update(on_on_update)
+
+def on_update_interval():
+    pass
+game.on_update_interval(5000, on_update_interval)
--- main.ts
+++ main.ts
@@ -24,9 +24,15 @@ function createEnemy () {
     true
     )
     tiles.placeOnRandomTile(enemySprite1, sprites.dungeon.buttonOrangeDepressed)
+    enemySprite1.follow(mySprite, 50)
 }
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    music.siren.play()
+    game.over(false, effects.slash)
+})
 let enemySprite1: Sprite = null
-let mySprite = sprites.create(img`
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -52,8 +58,14 @@ true
 )
 tiles.setCurrentTilemap(tilemap`level1`)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 8))
+createEnemy()
 game.onUpdate(function () {
     controller.moveSprite(mySprite, 200, 200)
     scene.cameraFollowSprite(mySprite)
-    createEnemy()
+    if (game.runtime() >= 10000) {
+        game.over(true, effects.confetti)
+    }
+})
+game.onUpdateInterval(5000, function () {
+	
 })
